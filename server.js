061afd48996dda32018fab5e781164582a11f19e
Catherine Looper <loopercat@gmail.com>
2018-10-28 20:09:42 -0700

    added functioning weather api

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const cors = require('cors');
+const superagent = require('superagent');
 
 require('dotenv').config();
 
@@ -15,15 +16,27 @@ app.get('/', (request, response) => {
 })
 
 app.get('/location', (request, response) => {
-  const locationInfo = geoSearch(request.query.data);
-  response.send(locationInfo); 
+  getCoordinates(request.query.data)
+  .then(locationInfo => response.send(locationInfo))
+  .catch(error => handleError(error, response));
 });
 
-function geoSearch(query) {
-  const geoInfo = require('./data/geo.json');
-  const location = new Location(geoInfo.results[0]);
-  location.search_query = query;
-  return location;
+function handleError(error, response) {
+  console.error('__ERROR__', error);
+  if (response) response.status(500).send('oooooops');
+}
+
+function getCoordinates(query) {
+  const URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
+  return superagent.get(URL)
+  .then(data => {
+    if (!data.body.results.length) throw 'NO DATA AVAILABLE';
+    else {
+      let location = new Location(data.body.results[0]);
+      location.search_query = query;
+      return location;
+    }
+  })
 }
 
 function Location(data) {
@@ -32,4 +45,25 @@ function Location(data) {
   this.longitude = data.geometry.location.lng;
 }
 
+app.get('/weather', getWeather);
+
+function getWeather(request, response) {
+  const URL = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
+  return superagent.get(URL)
+  .then(result => {
+   const weatherInformation = [];
+   result.body.daily.data.forEach(day => {
+     const summary = new Weather(day);
+     weatherInformation.push(summary);
+   })
+   response.send(weatherInformation);
+   console.log('weatherInformation: ', weatherInformation);
+  })
+  .catch(error => handleError(error, response));
+}
+
+function Weather(day) {
+  this.forecast = day.summary;
+  this.time = new Date(day.time * 1000).toString().slice(0, 15);
+}
 app.listen(PORT, () => console.log(`App is listening on PORT: ${PORT}`));
